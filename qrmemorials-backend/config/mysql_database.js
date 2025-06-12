2a2fed238decfabaead3b2d4e4edd3eb15cb656f
akanksha089 <achaturvdi089@gmail.com>
2025-06-12 18:26:16 +0530

    savecode

--- qrmemorials-backend/config/mysql_database.js
+++ qrmemorials-backend/config/mysql_database.js
@@ -1,50 +1,50 @@
-   const mysql = require("mysql2/promise");
-   const dotenv = require("dotenv");
-   dotenv.config({ path: "backend/config/config.env" });
+  //  const mysql = require("mysql2/promise");
+  //  const dotenv = require("dotenv");
+  //  dotenv.config({ path: "backend/config/config.env" });
 
-   const mysqlPool = mysql.createPool({
-     host: "localhost",
-     user: "root",
-     password: "",
-     database: "nodejs",
-     waitForConnections: true,
-     connectionLimit: 10,
-     queueLimit: 0,
-   });
-   module.exports = mysqlPool;
+  //  const mysqlPool = mysql.createPool({
+  //    host: "localhost",
+  //    user: "root",
+  //    password: "",
+  //    database: "nodejs",
+  //    waitForConnections: true,
+  //    connectionLimit: 10,
+  //    queueLimit: 0,
+  //  });
+  //  module.exports = mysqlPool;
 
 
-//  const mysql = require("mysql2/promise");
-//  const dotenv = require("dotenv");
-//  dotenv.config({ path: "backend/config/config.env" });
+  const mysql = require("mysql2/promise");
+  const dotenv = require("dotenv");
+  dotenv.config({ path: "backend/config/config.env" });
 
-//  const mysqlPool = mysql.createPool({
-//    host: process.env.DB_HOST,
-//    user: process.env.DB_USER,
-//    password: process.env.DB_PASSWORD,
-//    database: process.env.DB_NAME,
-//    port: process.env.DB_PORT,
-//    waitForConnections: true,
-//    connectionLimit: 3,
-//    queueLimit: 0,
-//    connectTimeout: 10000, // 10 seconds
-//    ssl: {
-//      rejectUnauthorized: false
-//    }
+  const mysqlPool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 3,
+    queueLimit: 0,
+    connectTimeout: 10000, 
+    ssl: {
+      rejectUnauthorized: false
+    }
 
-//  });
-//  async function testConnection() {
-//    try {
-//      const connection = await mysqlPool.getConnection();
-//      console.log("Database connected successfully!");
-//      connection.release(); 
-//    } catch (err) {
-//      console.error("Database connection error: ", err.message);
-//    }
-//  }
+  });
+  async function testConnection() {
+    try {
+      const connection = await mysqlPool.getConnection();
+      console.log("Database connected successfully!");
+      connection.release(); 
+    } catch (err) {
+      console.error("Database connection error: ", err.message);
+    }
+  }
 
-//  testConnection();
+  testConnection();
 
-//  module.exports = mysqlPool;
+  module.exports = mysqlPool;
 
 
